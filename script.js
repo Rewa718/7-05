a906a3851010d731d50d0396a4eb2c63aa9727bb
Rewa Patil <>
2021-05-11 15:14:17 +0000

    correct indentation

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ function decide () {
   // convert age number to integer
   const age = parseInt(document.getElementById('age').value)
   const day = document.getElementById('day').value
- // if age is less than 18, add message
- if (( day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday') && (age < 18)) {
-   document.getElementById('answer').innerHTML = 'Time for school!'
- } else if (( day !== "Saturday" || day !== "Sunday") && (age >= 18)) {
-   document.getElementById('answer').innerHTML = 'Time to go to work'
- } else {
-   document.getElementById('answer').innerHTML = 'It is the weekend! You can now relax!'
- }
- }
+  // if age is less than 18, add message
+  if (( day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday') && (age < 18)){
+    document.getElementById('answer').innerHTML = 'Time for school!'
+  } else if (( day !== 'Saturday' || day !== 'Sunday') && (age >= 18)){
+    document.getElementById('answer').innerHTML = 'Time to go to work'
+  } else {
+    document.getElementById('answer').innerHTML = 'It is the weekend! You can now relax!'
+  } }
   
