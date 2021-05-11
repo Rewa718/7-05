27b68b36da4b6de2f75f7c2eb9363f14583a210c
Rewa Patil <>
2021-05-11 15:07:55 +0000

    code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function decide () {
   const age = parseInt(document.getElementById('age').value)
   const day = document.getElementById('day').value
  // if age is less than 18, add message
- if (( day === "Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday" || day === "Friday") && (age < 18)) {
+ if (( day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday') && (age < 18)) {
    document.getElementById('answer').innerHTML = 'Time for school!'
  } else if (( day !== "Saturday" || day !== "Sunday") && (age >= 18)) {
    document.getElementById('answer').innerHTML = 'Time to go to work'
